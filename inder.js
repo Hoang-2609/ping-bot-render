a1f6ed035ae37aaa1dd545d6d2d201af3fc2ac71
Hoang-2609 <huhh16702@gmail.com>
2025-07-18 14:07:54 +0700

    Update inder.js

--- inder.js
+++ inder.js
@@ -1,14 +1,24 @@
+const express = require("express");
 const axios = require("axios");
 
-const BOT_URL = "https://08dcc9c4-b519-4324-91b6-53118c61e61e-00-46zd7avli2xn.sisko.replit.dev/"; // <- Thay link Replit bot bạn ở đây
+const app = express();
+const BOT_URL = process.env.BOT_URL || "https://bot.duohoagroblox.replit.dev"; // Thay link bot
 
 function pingBot() {
   const time = new Date().toLocaleTimeString();
   axios.get(BOT_URL)
-    .then(() => console.log(`[${time}] ✅ Ping thành công`))
+    .then(() => console.log(`[${time}] ✅ Ping bot ok`))
     .catch(err => console.log(`[${time}] ❌ Ping lỗi: ${err.message}`));
 }
 
-pingBot(); // Ping khi khởi động
+pingBot();
+setInterval(pingBot, 240000); // 4 phút
 
-setInterval(pingBot, 240000); // Ping mỗi 4 phút
+app.get("/", (req, res) => {
+  res.send("Ping bot đang chạy...");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server chạy trên port ${port}`);
+});
